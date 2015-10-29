7379499de8cdf0b8bebf412d988735e399e4107c
Chris McCammon <cmccammon@gmail.com>
2015-10-29 09:51:25 -0500

    generated beers controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+<<<<<<< Updated upstream
   devise_for :users, :controllers => { registrations: 'registrations' }
+=======
+  get 'beers/index'
+
+  get 'beers/show'
+
+>>>>>>> Stashed changes
   resources :posts
 
   root to: "posts#index"
