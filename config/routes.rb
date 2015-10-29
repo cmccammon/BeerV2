a74fbd661a42931f11bade8318c2285923a4a4ea
Chris McCammon <cmccammon@gmail.com>
2015-10-29 09:52:27 -0500

    fixed conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
   devise_for :users, :controllers => { registrations: 'registrations' }
 =======
+=======
+>>>>>>> Stashed changes
   get 'beers/index'
 
   get 'beers/show'
 
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
   resources :posts
 
