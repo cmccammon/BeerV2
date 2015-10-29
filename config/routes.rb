459de0e85f9938698419b6153cd441010413a757
Chris McCammon <cmccammon@gmail.com>
2015-10-29 10:02:15 -0500

    fixing materialize

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,10 @@
 Rails.application.routes.draw do
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-  devise_for :users, :controllers => { registrations: 'registrations' }
-=======
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-  get 'beers/index'
 
+  devise_for :users, :controllers => { registrations: 'registrations' }
+
+  get 'beers/index'
   get 'beers/show'
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
   resources :posts
 
   root to: "posts#index"
