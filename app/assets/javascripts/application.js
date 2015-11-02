d9634ff046be038ad0e2aab812512e033cf331e4
Chris McCammon <cmccammon@gmail.com>
2015-11-02 16:43:05 -0600

    saving progress on welcome page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,8 @@
 //= require_tree .
 
 $(".button-collapse").sideNav();
+
+$(document).ready(function(){
+  // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+  $('.modal-trigger').leanModal();
+});
