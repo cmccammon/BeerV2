eebd700e60ca9e7ef903b9d3fd83bedfb04fd660
Chris McCammon <cmccammon@gmail.com>
2016-05-11 09:25:40 -0500

    adding a user profile view

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,7 +2,6 @@ class ProfileController < ApplicationController
   def index
     @user_posts = current_user.posts.order('created_at DESC')
     @user = current_user
-
   end
 
   def show
--- app/models/beers.rb
+++ app/models/beers.rb
@@ -1,3 +1,1 @@
-def brewery_name(n) #I think I'm using this wrong. I want this method to pull the data I want out of the hash.
-  n.to_s
-end
+
